318f505d917b94bc09ce9cba98d8e771e9d2b8df
stmeem <stmeem@yahoo.com>
2022-04-23 22:55:19 +0600

    Fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({articles}) {
+  console.log(articles)
   return (
     <div>
       <Head>
@@ -9,4 +10,14 @@ export default function Home() {
       </Head>
     </div>
   )
+}
+
+export const getStaticProps = async()=>{
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`) 
+  const articles= await res.json()
+  return{
+    props:{
+      articles
+    }
+  }
 }
